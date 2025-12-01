197c8d8494118e4a777224b03e560382ff45c2ad
Rocio Aguilera <rocioaguilera214@gmail.com>
2025-12-01 18:05:16 -0300

    borrar comentarios

--- app/services/universidad_service.py
+++ app/services/universidad_service.py
@@ -1,6 +1,6 @@
 from app.models import Universidad
 from app.repositories import UniversidadRepository
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict, Any   
 import math
 import logging
 
@@ -13,17 +13,7 @@ class UniversidadService:
 
     @staticmethod
     def listar_universidades(page: int = 1, per_page: int = 10, filters: Optional[List[Dict[str, Any]]] = None) -> Dict[str, Any]:
-        """
-        Lista universidades, aplica paginación, y devuelve los metadatos asociados.
-
-        Args:
-            page: Número de página (base 1).
-            per_page: Cantidad de elementos por página.
-            filters: Lista de diccionarios en formato sqlalchemy-filters.
-
-        Returns:
-            Un diccionario con la lista de universidades en 'content' y los metadatos de paginación.
-        """
+        
         universidades: List[Universidad] = UniversidadRepository.listar_universidades(page, per_page, filters)
         total_elements: int = UniversidadRepository.contar_universidades(filters)
 
--- test/test_resource_universidad.py
+++ test/test_resource_universidad.py
@@ -25,5 +25,5 @@ class UniversidadResourceTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         universidades = response.get_json()
         self.assertIsNotNone(universidades)
-        self.assertIsInstance(universidades, list)
+        self.assertIsInstance(universidades, dict)
         
