b8c54cd95058328fc1bc2db837b23987c6e23f75
celesc16 <celechoquevillca2004@gmail.com>
2025-12-03 16:46:31 -0300

    Se modifica nombre

--- test/test_resource_especialidad.py
+++ test/test_resource_especialidad.py
@@ -127,7 +127,7 @@ class EspecialidadResourceTestCase(unittest.TestCase):
         data = response.get_json()
         self.assertEqual(data["letra"], "A")
 
-    def test_crear_especialidad(self):
+    def test_post_especialidad(self):
         payload = {
             "nombre": "Especialidad C",
             "letra": "C",
